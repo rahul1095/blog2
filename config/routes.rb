4ff5ce4e8a636e65ab12eb4c9e3decfaa030e645
Rahul <jain.rahul1095@gmail.com>
2017-04-10 18:38:38 +0530

    change gem file sqlid3 to pg

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   #get 'welcome/index'
   resources:blogs
-root 'welcome#index'
+root 'blogs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
