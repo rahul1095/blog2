1727e0ff3e25e963bd8e208238b21d5b3eb4a097
Rahul <jain.rahul1095@gmail.com>
2017-04-10 16:19:16 +0530

    added my profile

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,32 @@
+class BlogsController < ApplicationController
+	before_action :authenticate_user!
+	def index
+		@blogs=Blog.all
+	end
+	def new
+		@blog=Blog.new
+	end
+	def create
+		@blog=Blog.new(blog_params)
+		@blog.save
+		redirect_to @blog
+	end
+	def show
+		@blog=Blog.find(params[:id])
+     end
+
+      def edit
+	@blog=Blog.find(params[:id])
+
+	 end
+
+	def update
+		@blog=Blog.find(params[:id])
+		@blog.update(blog_params)
+	 end
+private
+  def blog_params
+    params.require(:blog).permit(:title, :description, :image)
+  end
+
+end
